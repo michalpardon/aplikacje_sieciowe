bc97614853e3f078ae0bc88ba2f474107c8f298d
michalpardon <109202929+michalpardon@users.noreply.github.com>
2022-10-30 08:54:12 +0100

    Delete php_06_kontroler_glowny_zadanie directory

--- php_06_kontroler_glowny_zadanie/app/calc/CalcResult.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-class CalcResult {
-	public $total;
-	public $result;	
-} 
--- php_06_kontroler_glowny_zadanie/app/calc_kredytowy.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/../config.php';
-//require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
-require_once $conf->root_path.'/app/CalcCtrl.class.php';
-$ctrl = new CalcCtrl();
-$ctrl->process();
-?>
