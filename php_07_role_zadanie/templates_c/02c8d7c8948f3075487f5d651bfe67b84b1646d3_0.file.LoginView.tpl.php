cb7165a5eb2bb5f675921c230d81efcc4611af64
pardonmichal <michal.pardon@gmail.com>
2022-11-16 14:11:12 +0100

    7a. ochrona zasobów na podstawie roli

---
+++ php_07_role_zadanie/templates_c/02c8d7c8948f3075487f5d651bfe67b84b1646d3_0.file.LoginView.tpl.php
@@ -0,0 +1,98 @@
+<?php
+/* Smarty version 4.2.1, created on 2022-11-13 17:54:53
+  from 'C:\xampp\htdocs\php_07_role_zadanie\app\views\LoginView.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.1',
+  'unifunc' => 'content_6371215d6eb252_03077675',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '02c8d7c8948f3075487f5d651bfe67b84b1646d3' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_07_role_zadanie\\app\\views\\LoginView.tpl',
+      1 => 1668358491,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:messages.tpl' => 1,
+  ),
+),false)) {
+function content_6371215d6eb252_03077675 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_6328697586371215d6e1a90_60223418', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_14799177196371215d6e35c1_65586312', 'content');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, "main.tpl");
+}
+/* {block 'footer'} */
+class Block_6328697586371215d6e1a90_60223418 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_6328697586371215d6e1a90_60223418',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+Michał Pardon<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_14799177196371215d6e35c1_65586312 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_14799177196371215d6e35c1_65586312',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div style="width:90%; margin: 2em auto;">
+<section>
+        <h3>Podaj dane do logowania</h3>
+        <form method="post" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+login">
+                <div class="row gtr-uniform gtr-50">
+                        <div class="col-6 col-12-xsmall">
+                            <input id="id_login" type="text" name="login" placeholder="Login" />
+                        </div>
+                        <div class="col-6 col-12-xsmall">
+                               <input id="id_pass" type="password" name="pass" placeholder="Hasło" />
+                        </div>
+                        <div class="col-12">
+                                <ul class="actions">
+                                        <li><input type="submit" value="Zaloguj" class="primary" /></li>
+                                </ul>
+                        </div>
+                </div>
+        </form>
+</section>
+
+
+<?php $_smarty_tpl->_subTemplateRender('file:messages.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+</div>
+<?php
+}
+}
+/* {/block 'content'} */
+}
